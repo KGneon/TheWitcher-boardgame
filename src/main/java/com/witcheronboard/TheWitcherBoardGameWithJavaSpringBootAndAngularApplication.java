269347110eb88f9e69c568af609eb853a825bbd1
Kami G <kam.gor45@gmail.com>
2023-09-19 13:00:39 +0200

    First try

---
+++ src/main/java/com/witcheronboard/TheWitcherBoardGameWithJavaSpringBootAndAngularApplication.java
@@ -0,0 +1,13 @@
+package com.witcheronboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheWitcherBoardGameWithJavaSpringBootAndAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheWitcherBoardGameWithJavaSpringBootAndAngularApplication.class, args);
+	}
+
+}
