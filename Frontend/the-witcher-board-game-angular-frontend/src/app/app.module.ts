eeefc0907c43a6e7519c5ccdc8c798d0c28764d2
Kami G <kam.gor45@gmail.com>
2023-10-05 01:54:04 +0200

    Finishing basic backend starting frontend

--- Frontend/the-witcher-board-game-angular-frontend/src/app/app.module.ts
+++ Frontend/the-witcher-board-game-angular-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { SettingsComponent } from './settings/settings.component';
+import { PlayGameComponent } from './play-game/play-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    SettingsComponent,
+    PlayGameComponent
   ],
   imports: [
     BrowserModule,
